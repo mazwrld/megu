941c36ed08c3dd95771c30ef458d655953bbbcfa
Maz <mazspm@icloud.com>
2024-01-14 19:04:16 -0600

    adds nav

---
+++ components/navbar.tsx
@@ -0,0 +1,85 @@
+import { Icons } from '@/components/icons'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import Link from 'next/link'
+import { Button } from './ui/button'
+
+export default function Navbar({}): any {
+  return (
+    <nav className="w-full backdrop-blur-md bg-white bg-opacity-30 z-50 fixed h-24 flex justify-between items-center py-10 px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-24">
+      <div className="gap-1 md:gap-2 lg:gap-4 hidden md:flex">
+        <Button variant="ghost" className="font-semibold text-md">
+          <Link href="/">Home</Link>
+        </Button>
+        <Link href="/about">
+          <Button variant="ghost" className="font-semibold text-md">
+            About
+          </Button>
+        </Link>
+        <Link href="/privacy-policy">
+          <Button variant="ghost" className="font-semibold text-md">
+            Privacy Policy
+          </Button>
+        </Link>
+      </div>
+      <Link href="">
+        <Button
+          variant="default"
+          className="rounded-full w-fit bg-orange-600 gap-2 items-center hidden md:flex"
+          size="lg"
+        >
+          <span>Github Repo</span>
+          <span className="text-xl">
+            <Icons.gitHub className="h-5 w-5" />
+          </span>
+        </Button>
+      </Link>
+
+      {/* MOBILE NAV */}
+      <Sheet>
+        <SheetTrigger className="block md:hidden p-3">
+          <span className="text-2xl">
+            <Icons.gitHub className="h-5 w-5" />
+          </span>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetDescription>
+              <div className="w-full space-y-3">
+                <Link href="/">
+                  <Button
+                    variant="link"
+                    className="font-semibold text-md w-full"
+                  >
+                    Home
+                  </Button>
+                </Link>
+                <Link href="/about">
+                  <Button
+                    variant="link"
+                    className="font-semibold text-md w-full"
+                  >
+                    About
+                  </Button>
+                </Link>
+                <Link href="/privacy-policy">
+                  <Button
+                    variant="link"
+                    className="font-semibold text-md w-full"
+                  >
+                    Privacy Policy
+                  </Button>
+                </Link>
+              </div>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </nav>
+  )
+}
